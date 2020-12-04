1ff41cbba1f7fc214fff94c9710afa0e4142ffa6
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-04 08:44:59 -0500

    better jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	private Rigidbody playerRb;
+	public float jumpForce = 200;
+	public float gravityModifier = 1;
 	private void Start()
 	{
 		playerRb = GetComponent<Rigidbody>();
+		Physics.gravity *= gravityModifier;
 	}
 
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Space) || (Application.isEditor && Input.GetKeyDown(KeyCode.O)))
 		{
-			playerRb.AddForce(Vector3.up * 1000);
+			playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 
 		}
 	}
