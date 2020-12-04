8761f66f80ec147a9e147d9b369494d0fba32b9e
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-04 08:37:43 -0500

    add basic jump at start of game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	private Rigidbody playerRb;
 	private void Start()
 	{
-
+		playerRb = GetComponent<Rigidbody>();
+		playerRb.AddForce(Vector3.up * 1000);
 	}
 
 	private void Update()
