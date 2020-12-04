fd0928d6de845a916400b5026640f4cdfec31b08
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-04 17:18:05 -0500

    more boxes at interval

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+	public GameObject obstaclePrefab;
+	private Vector3 spawnPos = new Vector3(30, 0, 0);
+
+	private float startDelay = 2;
+	private float repeatRate = 2;
+	private void Start()
+	{
+		InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+	}
+
+	private void SpawnObstacle()
+	{
+		Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+	}
+
+	private void Update()
+	{
+
+	}
+}
