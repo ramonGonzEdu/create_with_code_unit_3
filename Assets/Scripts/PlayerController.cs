2b670c08d9a82a7533e9763ce8ec71cae8bd6f25
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-04 08:49:19 -0500

    noDoubleJump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 	private Rigidbody playerRb;
 	public float jumpForce = 200;
 	public float gravityModifier = 1;
+	public bool isOnGround = true;
 	private void Start()
 	{
 		playerRb = GetComponent<Rigidbody>();
@@ -14,10 +15,15 @@ public class PlayerController : MonoBehaviour
 
 	private void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Space) || (Application.isEditor && Input.GetKeyDown(KeyCode.O)))
+		if ((Input.GetKeyDown(KeyCode.Space) || (Application.isEditor && Input.GetKeyDown(KeyCode.O))) && isOnGround)
 		{
 			playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
+			isOnGround = false;
 		}
+	}
+
+	void OnCollisionEnter()
+	{
+		isOnGround = true;
 	}
 }
