7fb15f2a51b471f43ecdc70b94e8cc3f07dad49b
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-18 12:30:58 -0500

    stop moving objects on game end

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
 	private float speed = 30;
+	private PlayerController playerControllerScript;
 	private void Start()
 	{
-
+		playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 	}
 
 	private void Update()
 	{
-		transform.Translate(Vector3.left * Time.deltaTime * speed);
+		if (!playerControllerScript.gameOver)
+			transform.Translate(Vector3.left * Time.deltaTime * speed);
 	}
 }
