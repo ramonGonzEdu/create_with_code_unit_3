0d1bcc7b770772c690043529635617d3afba8155
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-18 12:19:28 -0500

    add basic loopion

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+	private Vector3 startPos;
 	private void Start()
 	{
-
+		startPos = transform.position;
 	}
 
 	private void Update()
 	{
-
+		if (transform.position.x < startPos.x - 50)
+		{
+			transform.position = startPos;
+		}
 	}
 }
