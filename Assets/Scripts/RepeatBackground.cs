e4719348f611d5bd7e44ec76b032abcd3777334f
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-18 12:24:30 -0500

    smoother travel

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
 	private Vector3 startPos;
+	private float repeadWidth;
 	private void Start()
 	{
 		startPos = transform.position;
+		repeadWidth = GetComponent<BoxCollider>().size.x / 2;
 	}
 
 	private void Update()
 	{
-		if (transform.position.x < startPos.x - 50)
+		if (transform.position.x < startPos.x - repeadWidth)
 		{
 			transform.position = startPos;
 		}
