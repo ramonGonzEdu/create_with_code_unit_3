ecf9b27b3a80083db2cef23320d85282391a9e9b
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2020-12-18 12:34:49 -0500

    kill old objects

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,7 @@ public class MoveLeft : MonoBehaviour
 {
 	private float speed = 30;
 	private PlayerController playerControllerScript;
+	private float leftBound = -15;
 	private void Start()
 	{
 		playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
@@ -13,5 +14,8 @@ public class MoveLeft : MonoBehaviour
 	{
 		if (!playerControllerScript.gameOver)
 			transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+		if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+			Destroy(gameObject);
 	}
 }
